bcfdccca7a5997f60704b21366eef9617dfc64c3
palich <rautaruukkipalich@gmail.com>
2024-04-12 20:08:07 +0500

    add send messages and rework logic

--- internal/app/smtp/app.go
+++ internal/app/smtp/app.go
@@ -1,52 +1,120 @@
 package smtp
 
 import (
+	"fmt"
 	"log/slog"
+	"net/smtp"
 
 	"github.com/rautaruukkipalich/go_auth_grpc_smtp/internal/app/kafka"
 	"github.com/rautaruukkipalich/go_auth_grpc_smtp/internal/config"
+	"github.com/rautaruukkipalich/go_auth_grpc_smtp/internal/lib/slerr"
+)
+
+const (
+	EmptyString = ""
+)
+
+var (
+	ErrEmptyEmail = fmt.Errorf("empty email address")
+	ErrEmptyMessage = fmt.Errorf("empty message")
 )
 
 type SMTPClient struct {
 	log *slog.Logger
-
+	done chan struct{}
+	auth *smtp.Auth	
+	addr string
+	from string
 }
 
-func New(log *slog.Logger, cfg *config.Config) *SMTPClient {
+func New(log *slog.Logger, cfg *config.SMTPConfig) *SMTPClient {
+	
+	auth := smtp.PlainAuth("", cfg.From, cfg.Pass, cfg.Host)
+	done := make(chan struct{})
+
+
 	return &SMTPClient{
 		log: log,
+		done: done,
+		auth: &auth,
+		addr: cfg.Addr,
+		from: cfg.From,
 	}
 }
 
-func (c *SMTPClient) Run(msgch chan kafka.Message) {
-	c.log.Info("start SMTP client")
+func (c *SMTPClient) Run(msgch chan kafka.Payload) {
+	const op = "app.smtp.app.Run"
+	log := c.log.With(slog.String("op", op))
+	log.Info("start SMTP client")
 
 	for {
 		select {
 			case data, ok :=<- msgch:
 				if !ok {
-					continue
+					c.log.Error("channel closed")
 				}
-				if err := c.SendMessage(&data); err != nil {
+				if err := c.sendMessage(&data); err != nil {
 					c.log.Error("send message")
 				}
-			default:
-				continue
+			case <- c.done:
+				return
 		}
 	}
 	
 }
 
 func (c *SMTPClient) Stop() {
-	panic("stop")
+	const op = "app.smtp.app.Run"
+	log := c.log.With(slog.String("op", op))
+	log.Info("stop SMTP client")
+
+	defer close(c.done)
 }
 
-func (c *SMTPClient) SendMessage(msg *kafka.Message) error {
-	const op = "app.smtp.app.SendMessage"
+func (c *SMTPClient) sendMessage(msg *kafka.Payload) error {
+	const op = "app.smtp.app.sendMessage"
 	log := c.log.With(slog.String("op", op))
-	log.With(slog.String("email", msg.Payload.Email)).Info("send message")
+	log.With(slog.String("email", msg.Email)).Info("send message")
+
+	email, payload, err := c.prepareMessage(msg)
+	if err != nil {
+		log.Error("error prepare message", op, slerr.Err(err))
+		return err
+	}
 	
-	// TODO
+	if err := smtp.SendMail(c.addr, *c.auth, c.from, []string{email}, []byte(payload)); err != nil {
+		log.Error("error sending message", op, slerr.Err(err))
+		return err
+	}
 
 	return nil
 }
+
+func (c *SMTPClient) prepareMessage(msg *kafka.Payload) (email, payload string, err error) {
+	const op = "app.smtp.app.prepareMessage"
+	log := c.log.With(slog.String("op", op))
+	log.With(slog.String("email", msg.Email)).Info("prepare message")
+	
+	if msg.Email == EmptyString {
+		log.Error("error prepare email", op, slerr.Err(ErrEmptyEmail))
+		return "", "", fmt.Errorf("%s: %w", op, ErrEmptyEmail)
+	}
+
+	if msg.Message == EmptyString {
+		log.Error("error prepare message", op, slerr.Err(ErrEmptyMessage))
+		return "", "", fmt.Errorf("%s: %w", op, ErrEmptyMessage)
+	}
+
+	message := fmt.Sprintf(
+		"To: %s\r\n"+
+		"Subject: %s\r\n\r\n"+
+		"You can use this password to log in: %s\nDo not forget to edit your password\r\n",
+		 msg.Email,
+		 msg.Header,
+		 msg.Message,
+	)
+
+	return msg.Email, message, nil
+}
+
+
